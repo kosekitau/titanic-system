f84cf48ae0dcc33405d597fbce226914e4d775a1
koseki <kosekitau@gmail.com>
2026-03-22 13:44:41 +0900

    alembicを使えるようにした

---
+++ src/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "persons"
+    id = Column(Integer, primary_key=True)
+    pclass = Column(Integer)
+    sex = Column(String(255))
+    age = Column(Integer)
+    slibSp = Column(Integer)
+    parch = Column(Integer)
+    ticket = Column(String(255))
+    fare = Column(Float)
+    cabin = Column(String(255))
+    embarked = Column(String(255))
+
+    def __repr__(self):
+        return "<Person(id='%s', pclass='%s', sex='%s', age='%s')>" % (self.id, self.pclass, self.sex, self.age)
